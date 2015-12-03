c40a2a32b9fc673cff04f344655f024644004953
Víctor J. Marín <victorjmarin@gmail.com>
2015-12-03 13:16:32 +0100

    Integración con almacenamiento mejorada
    Ahora se hace uso de la librería Retrofit para ello. Paulatinamente se irá mejorando la abstracción de la implementación de los servicios de integración.

--- counting/src/main/java/es/us/agoraus/counting/controllers/ApiTestController.java
+++ counting/src/main/java/es/us/agoraus/counting/controllers/ApiTestController.java
@@ -9,25 +9,27 @@ import javax.crypto.BadPaddingException;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import es.us.agoraus.counting.algorithms.Algoritmo;
 import es.us.agoraus.counting.domain.Resultado;
 import es.us.agoraus.counting.domain.VotoAntiguo;
 import es.us.agoraus.counting.domain.VotosCifrados;
+import es.us.agoraus.counting.integration.StorageServiceImpl;
 
 @RestController
 public class ApiTestController {
+	
+	@Autowired
+	StorageServiceImpl storageService;
 
 	@RequestMapping("/welcome")
-	public String welcome(
-			@RequestParam(value = "name", required = false, defaultValue = "people") String name) {
-		String welcome = "Hello "
-				+ name
+	public String welcome(@RequestParam(value = "name", required = false, defaultValue = "people") String name) {
+		String welcome = "Hello " + name
 				+ ", welcome to our API Test, yours can use this URL for get your name in the sentence:"
 				+ " http://localhost:8080/test/welcome?name=YourName";
 
@@ -35,8 +37,7 @@ public class ApiTestController {
 	}
 
 	@RequestMapping("/test")
-	public VotoAntiguo test(
-			@RequestParam(value = "name", required = false, defaultValue = "John Dou") String name) {
+	public VotoAntiguo test(@RequestParam(value = "name", required = false, defaultValue = "John Dou") String name) {
 		VotoAntiguo voto = new VotoAntiguo();
 		voto.setNombre(name);
 		voto.setEdad(35);
@@ -51,19 +52,13 @@ public class ApiTestController {
 	}
 
 	@RequestMapping("/recuento3")
-	public List<Resultado> recuento3(
-			@RequestParam(value = "idVotacion", required = true) String idVotacion)
-			throws BadPaddingException, InvalidKeyException,
-			NoSuchAlgorithmException, InvalidKeySpecException,
+	public List<Resultado> recuento3(@RequestParam(value = "idVotacion", required = true) String pollId)
+			throws BadPaddingException, InvalidKeyException, NoSuchAlgorithmException, InvalidKeySpecException,
 			NoSuchPaddingException, IllegalBlockSizeException {
 
-		RestTemplate restTemplate = new RestTemplate();
-		VotosCifrados votos = restTemplate.getForObject(
-				"http://php-egc.rhcloud.com/get_votes.php?votation_id="
-						+ idVotacion, VotosCifrados.class);
+		VotosCifrados votos = storageService.getVotesForPoll(pollId);
 
-		List<Resultado> resultados = Algoritmo.algoritmo3(idVotacion,
-				votos.getVotes());
+		List<Resultado> resultados = Algoritmo.algoritmo3(pollId, votos.getVotes());
 		return resultados;
 
 	}
