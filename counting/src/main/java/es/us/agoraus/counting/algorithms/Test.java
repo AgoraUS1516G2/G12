dc5f25a6dc7cfa3a9cfc916013ad2d07008080d3
rafaeldrs <rafriosan@gmail.com>
2015-12-20 10:46:42 +0100

    Modificada la obtencion del valor del token
    En este commit se ha modificado la obtencion del valor del token para
    los recuentos ejecutados en las pruebas.

--- counting/src/main/java/es/us/agoraus/counting/algorithms/Test.java
+++ counting/src/main/java/es/us/agoraus/counting/algorithms/Test.java
@@ -10,15 +10,19 @@ public class Test {
  
 	public static List<Resultado> referendumAlgorithmTestVotation() throws Exception {
 		
+		Integer pollId;
+		String pollIdStr;
 		Integer token;
 		AuthorityImpl auth;
 		List<Resultado> result;
 		List<byte[]> encryptedVotes;
 		
+		pollId = 1;
+		pollIdStr = String.valueOf(pollId);
 		encryptedVotes = new ArrayList<byte[]>();
 		result= new ArrayList<Resultado>();
 		auth = new AuthorityImpl();
-		token = Token.calculateToken(2);
+		token = Token.calculateToken(pollId);
 		
 		String vote1 = "{\"age\": \"24\",\"answers\":[{\"question\":\"¿Es usuario de linux?\",\"answer_question\":\"SI\"}"
                 + "],\"id\": 1,\"autonomous_community\": \"Andalucia\",\"genre\": \"Masculino\",\"id_poll\": 32778}";
@@ -34,17 +38,17 @@ public class Test {
         
         
 
-        byte[] encryptedVote1 = auth.encrypt("1", vote1, token);
-        byte[] encryptedVote2 = auth.encrypt("1", vote2, token);
-        byte[] encryptedVote3 = auth.encrypt("1", vote3, token);
-        byte[] encryptedVote4 = auth.encrypt("1", vote4, token);
+        byte[] encryptedVote1 = auth.encrypt(pollIdStr, vote1, token);
+        byte[] encryptedVote2 = auth.encrypt(pollIdStr, vote2, token);
+        byte[] encryptedVote3 = auth.encrypt(pollIdStr, vote3, token);
+        byte[] encryptedVote4 = auth.encrypt(pollIdStr, vote4, token);
         
         encryptedVotes.add(encryptedVote1);
         encryptedVotes.add(encryptedVote2);
         encryptedVotes.add(encryptedVote3);
         encryptedVotes.add(encryptedVote4);
         
-        result = Algoritmo.naturalCountingAlgorithm("1", encryptedVotes);
+        result = Algoritmo.naturalCountingAlgorithm(pollIdStr, encryptedVotes);
 
         return result;
 	
